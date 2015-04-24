17be017abb71224d0b298c1b33270d07947ddd05
stdStringPelczar <pawel.p.pelczar@gmail.com>
2015-04-25 00:04:44 +0200

    Display and option selection
    Overhaul of option selection and display, minor display changes,
    TODO display selection

--- display.js
+++ display.js
@@ -4,12 +4,14 @@ display.mainText = new Object();
 display.mainText._counter = 0;
 display.mainText._contentToDisplay = new String();
 display.mainText._intervalFuncHandler;
+display.mainText._interval = 10;
+
 
 display.mainText.write = function(str)
 {
 	display.mainText._counter = 0;
-	display.mainText._contentToWrite = str;
-	display.mainText._intervalFuncHandler = setInterval(display.mainText._writeByChar, 30);
+	display.mainText._contentToDisplay = str;
+	display.mainText._intervalFuncHandler = setInterval(display.mainText._writeByChar, display.mainText._interval);
 }
 
 
@@ -25,3 +27,18 @@ display.mainText._writeByChar = function()
 	}
 	document.getElementById("mainTextField").innerHTML = display.mainText._contentToDisplay.substring(0, display.mainText._counter);
 }
+
+
+display.mainText.setInterval = function( i )
+{
+	if (i >= 0)
+	{
+		display.mainText._interval = i;
+	}
+	else
+	{
+		console.log("Nieprawidlowa wartosc!");
+	}
+	
+}
+
