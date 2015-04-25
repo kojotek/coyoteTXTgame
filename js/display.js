b0cc6a43b1d15550397a9c86d694632ef7744892
Przemyslaw <dargemir@wp.pl>
2015-04-25 19:28:47 +0200

    CSS wydzielony do pliku, dodane foldery, obcinanie opcji

--- js/display.js
+++ js/display.js
@@ -6,13 +6,13 @@ display.mainText._contentToDisplay = new String();
 display.mainText._intervalFuncHandler;
 display.mainText._interval = 10;
 
+
 display.mainText.write = function(str)
 {
 	display.mainText._counter = 0;
 	display.mainText._contentToDisplay = str;
 	display.mainText._intervalFuncHandler = setInterval(display.mainText._writeByChar, display.mainText._interval);
 }
-
 
 display.mainText._writeByChar = function()
 {
@@ -27,7 +27,6 @@ display.mainText._writeByChar = function()
 	document.getElementById("mainTextField").innerHTML = display.mainText._contentToDisplay.substring(0, display.mainText._counter);
 }
 
-
 display.mainText.setInterval = function( i )
 {
 	if (i >= 0)
@@ -41,6 +40,10 @@ display.mainText.setInterval = function( i )
 	
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 display.optionList = new Object();
 display._currentOption = 0;
 
@@ -48,9 +51,9 @@ display.optionList.add = function( option )
 {
 	var table = document.getElementById("optionList");
     var row = table.insertRow(table.rows.length);
+	row.className = "optionBoxContent notChosen";
     var cell = row.insertCell(0);
     cell.innerHTML = option.text;
-	cell.className = "optionNotChosen";
 }
 
 display.optionList.remove = function( option )
@@ -65,4 +68,33 @@ display.optionList.remove = function( option )
 		}
 	}
 	table.deleteRow(index);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+display.window = new Object();
+
+display.refresh = function()
+{
+	var optionTableHeight = document.getElementById("optionBox").offsetHeight;
+	var allOptions = document.getElementsByClassName("optionBoxContent");
+	var sum = 0;
+	
+	for( var i=0; i<allOptions.length; i++ ){
+		allOptions[i].style.display = "table";
+	}
+	
+	for( var i=0; i<allOptions.length; i++ )
+	{
+		sum = sum + allOptions[i].offsetHeight;
+		
+		if ( sum < optionTableHeight ){
+			allOptions[i].style.display = "table-row";
+		}
+		else{
+			allOptions[i].style.display = "none";
+		}
+	}
 }
