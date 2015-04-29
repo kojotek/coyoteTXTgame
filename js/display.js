f833b9a086e5d0004a47aea3bf214becf67a898d
Przemyslaw <dargemir@wp.pl>
2015-04-29 03:37:55 +0200

    Wybieranie opcji jest strasznie powolne, ale dziala

--- js/display.js
+++ js/display.js
@@ -1,123 +1,27 @@
 var display = new Object();
-
-display.mainText = new Object();
-display.mainText._counter = 0;
-display.mainText._contentToDisplay = new String();
-display.mainText._intervalFuncHandler;
-display.mainText._interval = 10;
-
-
-display.mainText.write = function(str)
-{
-	display.mainText._counter = 0;
-	display.mainText._contentToDisplay = str;
-	display.mainText._intervalFuncHandler = setInterval(display.mainText._writeByChar, display.mainText._interval);
-}
-
-display.mainText._writeByChar = function()
-{
-	if (display.mainText._counter < display.mainText._contentToDisplay.length){
-		display.mainText._counter++;
-	}
-	else{
-		clearInterval(display.mainText._intervalFuncHandler);
-	}
-	document.getElementById("mainTextField").innerHTML = display.mainText._contentToDisplay.substring(0, display.mainText._counter);
-}
-
-display.mainText.setInterval = function( i )
-{
-	if (i >= 0){
-		display.mainText._interval = i;
-	}
-	else{
-		console.log("Nieprawidlowa wartosc!");
-	}
-}
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-display.optionList = new Object();
-display.optionList._currentOption = 0;
-display.optionList._optionBoxExtended = false;
-
-display.optionList.add = function( option )
-{
-	var table = document.getElementById("optionTable");
-    var row = table.insertRow(table.rows.length);
-	row.className = "option";
-	row.tag = option.tag;
-	row.text = option.text;
-	display.window.refresh();
-}
-
-display.optionList.remove = function( tag )
-{
-	var table = document.getElementById("optionTable");
-	var indexToDelete = findWithAttr(table.rows, "tag", tag);
-	if (indexToDelete !== undefined){
-		table.deleteRow(indexToDelete);
-	}
-	display.window.refresh();
-}
-
-display.optionList.next = function()
-{
-	display.optionList._currentOption = Math.max( Math.min( display.optionList._currentOption+ 1, game.options.count()-1  ), 0 );
-	display.window.refresh();
-}
-
-display.optionList.prev = function()
-{
-	display.optionList._currentOption = Math.min( Math.max( display.optionList._currentOption-1, 0 ), game.options.count() );
-	display.window.refresh();
-}
-
-display.optionList.extend = function()
-{
-	display.optionList._optionBoxExtended = true;
-	display.window.refresh();
-	
-}
-
-display.optionList.shorten = function()
-{
-	display.optionList._optionBoxExtended = false;
-	display.window.refresh();
-}
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 display.window = new Object();
+
 
 display.window.refresh = function()
 {
-	var optionTableHeight = document.getElementById("optionBox").offsetHeight;
+	
 	var allOptions = document.getElementsByClassName("option");
+
+	display.window.reset();
+	display.window.markSelectedOption();
+	display.window.setOptionBoxMode();
+
+	
+/*inicjalizacja zmiennych potrzebnych do obliczen*/
+	var optionTableHeight = 0.88 * document.getElementById("optionBox").offsetHeight;
 	var sum = 0;
+	var visibleOptions = 0;
 	
-/*resetowanie ustawien*/
-	for( var i=0; i<allOptions.length; i++ ){
-		allOptions[i].style.display = "table-row";
-		allOptions[i].id = undefined;
-		allOptions[i].innerHTML = "&nbsp";
-	}
-	
-/*zaznaczanie wybranej opcji*/
-	allOptions[display.optionList._currentOption].id = "selectedOption"
-	
-/*Ramka opcji: wydluzona, czy zwykla*/
-	document.getElementById("optionBox").className = (display.optionList._optionBoxExtended ? "optionBox extended" : "optionBox");
-	document.getElementById("optionTable").className = (display.optionList._optionBoxExtended ? "optionTable extended" : "optionTable");
 	
 	for( var i=0; i<allOptions.length; i++ )
-	{
-
-/*Ucinanie za dlugich opcji*/
+	{	
+		
+	/*Ucinanie za dlugich opcji*/
 		var lh = allOptions[i].offsetHeight;
 		var k = 1;
 		while ( k !== allOptions[i].text.length+1 ){
@@ -129,14 +33,122 @@ display.window.refresh = function()
 			}
 		}
 		
-/*Wyswietlanie tylko widocznych opcji*/
-		sum = sum + allOptions[i].offsetHeight;
 		
+	/*policz ile linii zmiesci sie w ramce*/
+		sum = sum + allOptions[i].offsetHeight;
 		if ( sum < optionTableHeight ){
-			allOptions[i].style.display = "table-row";
+			visibleOptions = i;
 		}
-		else{
-			allOptions[i].style.display = "none";
+		
+		
+	}
+	
+	
+/*przesun zakres zaleznie od polozenia currentOption*/
+	while( (display.optionList._upperLimit - display.optionList._lowerLimit) < visibleOptions ){
+		display.optionList._upperLimit++;
+	}
+	while( (display.optionList._upperLimit - display.optionList._lowerLimit) > visibleOptions ){
+		display.optionList._upperLimit--;
+	}
+	while( display.optionList._currentOption > display.optionList._upperLimit ){
+		display.optionList._lowerLimit++; display.optionList._upperLimit++;
+	}
+	while( display.optionList._currentOption < display.optionList._lowerLimit ){
+		display.optionList._lowerLimit--; display.optionList._upperLimit--;
+	}
+	
+
+/*wyswietl tylko te opcje, ktore mieszcza sie w zakresie*/
+	for( var i=0; i<display.optionList._lowerLimit; i++ ){
+		allOptions[i].style.display = "none";
+	}
+	for( var i=display.optionList._upperLimit+1; i<allOptions.length; i++ ){
+		allOptions[i].style.display = "none";
+	}
+
+}
+
+
+display.window.reset = function()
+{
+	var allOptions = document.getElementsByClassName("option");
+
+	for( var i=0; i<allOptions.length; i++ ){
+		allOptions[i].style.display = "table-row";
+		allOptions[i].id = undefined;
+		allOptions[i].innerHTML = "&nbsp";
+	}
+}
+
+
+display.window.markSelectedOption = function()
+{
+	var allOptions = document.getElementsByClassName("option");
+	allOptions[display.optionList._currentOption].id = "selectedOption";
+}
+
+
+display.window.setOptionBoxMode = function()
+{
+	document.getElementById("optionBox").className = (display.optionList._optionBoxExtended ? "optionBox extended" : "optionBox");
+	document.getElementById("optionTable").className = (display.optionList._optionBoxExtended ? "optionTable extended" : "optionTable");
+}
+
+
+display.window.drawOptions = function()
+{
+	/*inicjalizacja zmiennych potrzebnych do obliczen*/
+	var allOptions = document.getElementsByClassName("option");
+	var optionTableHeight = 0.88 * document.getElementById("optionBox").offsetHeight;
+	var sum = 0;
+	var visibleOptions = 0;
+	
+	
+	for( var i=0; i<allOptions.length; i++ )
+	{	
+	/*Ucinanie za dlugich opcji*/
+		var lh = allOptions[i].offsetHeight;
+		var k = 1;
+		while ( k !== allOptions[i].text.length+1 ){
+			allOptions[i].innerHTML = allOptions[i].text.substr(0,k);
+			k++;
+			if( allOptions[i].offsetHeight !== lh ){
+				allOptions[i].innerHTML = allOptions[i].text.substr(0,k-7) + "...";
+				break;
+			}
+		}
+		
+	/*policz ile linii zmiesci sie w ramce*/
+		sum = sum + allOptions[i].offsetHeight;
+		if ( sum < optionTableHeight ){
+			visibleOptions = i;
 		}
 	}
+	
+	
+/*przesun zakres zaleznie od polozenia currentOption*/
+	while( (display.optionList._upperLimit - display.optionList._lowerLimit) < visibleOptions ){
+		display.optionList._upperLimit++;
+	}
+	while( (display.optionList._upperLimit - display.optionList._lowerLimit) > visibleOptions ){
+		display.optionList._upperLimit--;
+	}
+	while( display.optionList._currentOption > display.optionList._upperLimit ){
+		display.optionList._lowerLimit++; display.optionList._upperLimit++;
+	}
+	while( display.optionList._currentOption < display.optionList._lowerLimit ){
+		display.optionList._lowerLimit--; display.optionList._upperLimit--;
+	}
+	
+
+/*wyswietl tylko te opcje, ktore mieszcza sie w zakresie*/
+	for( var i=0; i<display.optionList._lowerLimit; i++ ){
+		allOptions[i].style.display = "none";
+	}
+	for( var i=display.optionList._upperLimit+1; i<allOptions.length; i++ ){
+		allOptions[i].style.display = "none";
+	}
+	
+	
 }
---
+++ js/display_optionList.js
@@ -0,0 +1,56 @@
+display.optionList = new Object();
+display.optionList._currentOption = 0;
+display.optionList._optionBoxExtended = false;
+display.optionList._upperLimit = 0;
+display.optionList._lowerLimit = 0;
+
+
+display.optionList.add = function( option )
+{
+	var table = document.getElementById("optionTable");
+    var row = table.insertRow(table.rows.length);
+	row.className = "option";
+	row.tag = option.tag;
+	row.text = option.text;
+	display.window.refresh();
+}
+
+
+display.optionList.remove = function( tag )
+{
+	var table = document.getElementById("optionTable");
+	var indexToDelete = findWithAttr(table.rows, "tag", tag);
+	if (indexToDelete !== undefined){
+		table.deleteRow(indexToDelete);
+	}
+	display.window.refresh();
+}
+
+
+display.optionList.next = function()
+{
+	display.optionList._currentOption = Math.max( Math.min( display.optionList._currentOption+ 1, game.options.count()-1  ), 0 );
+	display.window.refresh();
+}
+
+
+display.optionList.prev = function()
+{
+	display.optionList._currentOption = Math.min( Math.max( display.optionList._currentOption-1, 0 ), game.options.count() );
+	display.window.refresh();
+}
+
+
+display.optionList.extend = function()
+{
+	display.optionList._optionBoxExtended = true;
+	display.window.refresh();
+	
+}
+
+
+display.optionList.shorten = function()
+{
+	display.optionList._optionBoxExtended = false;
+	display.window.refresh();
+}
